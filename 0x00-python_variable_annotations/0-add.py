4ee6235eedb6ae568f8b921e9a99fdaf0e22c9e4
kabakae <kabakaevans@gmail.com>
2024-08-01 10:43:55 +0000

    00

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+0-add Module
+============
+
+This module contains the function `add` which takes two floats as arguments
+and returns their sum as a float.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats and returns their sum.
+
+    Parameters:
+    -----------
+    a : float
+        The first float to add.
+    b : float
+        The second float to add.
+
+    Returns:
+    --------
+    float
+        The sum of the two floats.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+1-concat Module
+===============
+
+This module contains the function `concat` which takes two strings as arguments
+and returns their concatenated result.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+    -----------
+    str1 : str
+        The first string to concatenate.
+    str2 : str
+        The second string to concatenate.
+
+    Returns:
+    --------
+    str
+        The concatenated result of the two strings.
+    """
+    return str1 + str2
